891db163dfab2a6ba79894b2aac6c0e063b632d1
Ilshat Latypov <latypov.ilshat@gmail.com>
2019-09-12 21:31:15 +0300

    first commit

---
+++ www/FileChooserMultiple.js
@@ -0,0 +1,14 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'FileChooserMultiple', 'coolMethod', [arg0]);
+};
+
+module.exports.open = function (filter, success, failure) {
+    if (typeof filter === 'function') {
+        failure = success;
+        success = filter;
+        filter = {};
+    }
+    exec(success, failure, "FileChooser", "open", [ filter ]);
+};
