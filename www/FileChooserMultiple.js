4697d045790a4badfaf7f74ee1864b36d255f170
Ilshat Latypov <latypov.ilshat@gmail.com>
2019-09-13 14:02:20 +0300

    remove coolMethod

--- www/FileChooserMultiple.js
+++ www/FileChooserMultiple.js
@@ -1,9 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'FileChooserMultiple', 'coolMethod', [arg0]);
-};
-
 module.exports.open = function (arg0, success, error) {
     exec(success, error, 'FileChooserMultiple', 'open', [arg0]);   
 };
